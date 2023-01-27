f0a1631045482b29c420044268663c1bb55de92f
cmadajski <cmad7317@tutanota.com>
2023-01-26 21:59:02 -0500

    Use map to pass props to components

---
+++ src/cardData.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: "Card 1",
+        description: "Hello world, this is some text."
+    },
+    {
+        title: "Card 2",
+        description: "This is a generic description, but you keep reading it as if it is important?"
+    },
+    {
+        title: "Card 3",
+        description: "I keep on typing things that are nonsensical but it feels great!"
+    },
+    {
+        title: "Card 4",
+        description: "My brain is tired."
+    }
+]
--- src/index.js
+++ src/index.js
@@ -2,26 +2,18 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Navbar from "./Navbar"
 import Card from "./Card"
+import cardData from "./cardData"
 import "./index.css"
 import "./Navbar.css"
 import "./Card.css"
+
+const allCards = cardData.map(aCard => <Card title={aCard.title} description={aCard.description} />)
 
 ReactDOM.render(
     <div id="app-container">
         <Navbar />
         <div className="card-container">
-            <Card 
-                title="Please go away"
-                description="Nope, not doing it!"
-            />
-            <Card 
-                title="Herpy derpy"
-                description="I like melons..."
-            />
-            <Card 
-                title="Another card 4 u!"
-                description="Totally unique description"
-            />
+            {allCards}
         </div>
     </div>,
     document.getElementById("root")
