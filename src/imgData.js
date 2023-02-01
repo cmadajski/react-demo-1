8c67278a7095d45483be336385ed225efe3fff34
cmadajski <cmad7317@tutanota.com>
2023-02-01 14:33:52 -0500

    Add more pictures to imgData

--- src/imgData.js
+++ src/imgData.js
@@ -4,5 +4,15 @@ export const imgData = {
         id: 1,
         src: "http://www.nwostrats.com/img/photos/IMG_6853.JPG",
         caption: "Bumble bee on hairy vetch (Vicia villosa)."
+    },
+    2: {
+        id: 2,
+        src: "http://www.nwostrats.com/img/photos/IMG_8467.JPG",
+        caption: "Small North American brown snake (Storeria dekayi)."
+    },
+    3: {
+        id: 3,
+        src: "http://nwostrats.com/img/photos/IMG_7143.JPG",
+        caption: "Dragonfly resting on branch. Taken near UNC Charlotte campus."
     }
 }
