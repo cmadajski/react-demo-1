3017eb090258bacf1b2e9ce7656924b123e6ff3d
cmadajski <cmad7317@tutanota.com>
2023-02-01 16:27:06 -0500

    Add support for clickable prev/next buttons and keyboard arrow navigation

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -7,14 +7,34 @@ const Slideshow = (props) => {
 
     const [allImgData, setAllImgData] = useState(props.imgData)
     const [currImgId, setCurrImgId] = useState(props.start)
+    const numImgs = Object.keys(allImgData).length
 
     const currImgData = allImgData[currImgId]
 
+    const nextImg = () => {
+        setCurrImgId((prevId) => prevId === numImgs ? 1 : prevId + 1)
+
+    }
+
+    const prevImg = () => {
+        setCurrImgId((prevId) => prevId === 1 ? numImgs : prevId - 1)
+    }
+
+    const logKey = (event) => {
+        if (event.key === "ArrowRight") {
+            nextImg()
+        } else if (event.key === "ArrowLeft") {
+            prevImg()
+        } else {
+            
+        }
+    }
+
     return (
-        <div className="slideshow-container">
+        <div className="slideshow-container" tabIndex={0} onKeyDown={logKey}>
             <div className="current-img-container">
-                <div className="arrow arrow--backward">&larr;</div>
-                <div className="arrow arrow--forward">&rarr;</div>
+                <div className="arrow arrow--backward" onClick={prevImg}>&larr;</div>
+                <div className="arrow arrow--forward" onClick={nextImg}>&rarr;</div>
                 <img className="current-img" src={currImgData.src} />
                 <p className="caption">{currImgData.caption}</p>
             </div>
